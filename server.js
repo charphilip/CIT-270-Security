f4a7c8450fe07834c5bdb10bbe849dac8087a6e7
Felipe <charphilip@hotmail.com>
2022-09-30 13:20:52 -0600

    othersss

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-const {v4 : uuidve} = require("uuid");
+const {v4 : uuidv4} = require("uuid");
 const port = 3000;
 const app = express();
+const redis = require("redis");
+const md5 = require('md5');
+const {createClient} = require('redis');
+const redisClient = createClient(
+    {
+        Url:'redis://default@localhost:6379',
+    }
+)
 
 app.use(bodyParser.json());
 
 app.listen(port, async ()=>{
+    await redisClient.connect();
     console.log('listening on port '+port);
 });
 
@@ -20,6 +29,13 @@ app.post("/login", (req,res)=>{
     console.log("loginEmail", loginEmail);
     const loginPassword = req.body.password;
     console.log("loginPassword", loginPassword);
-    res.send("Who are you");
-    // if (loginEmail == "555@co5.com")
+    
+    //res.send("Who are you");
+    if (loginEmail == "555@co5.com" && loginPassword == "P@ssw0rd"){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized
+        res.send("Invalid user or password");
+    }
 })
